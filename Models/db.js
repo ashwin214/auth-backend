bc608f936913bcbbac9f6b50d66776a54195169c
ashwin goyal <goyalashwin2002@gmail.com>
2026-08-19 20:35:02 +0530

    Add fallback MONGO_URI for cloud deployment

--- Models/db.js
+++ Models/db.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
-const mongo_url = process.env.MONGO_URI;
+const mongo_url =
+  process.env.MONGO_URI ||
+  "mongodb+srv://goyalashwin2002_db_user:Goyal%40123@cluster0.9jdnob5.mongodb.net/auth-db?appName=Cluster0";
 mongoose
   .connect(mongo_url)
   .then(() => console.log("Connected to MongoDB"))
